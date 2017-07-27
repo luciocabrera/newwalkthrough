2805006bbd6d90fde4110bdcda162ef1d43b8ef2
luciocabrera <lucio.cabrera@just-bi.nl>
2017-07-27 10:57:37 +0200

    changing the DEtail controller, to  use the Basecontoller

--- controller/Detail.controller.js
+++ controller/Detail.controller.js
@@ -1,7 +1,6 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/core/routing/History"
-], function(Controller, History) {
+    "sap/ui/demo/wt/controller/BaseController"
+], function(BaseController) {
     "use strict";
     return Controller.extend("sap.ui.demo.wt.controller.Detail", {
         onInit: function() {
@@ -13,17 +12,6 @@ sap.ui.define([
                 path: "/" + oEvent.getParameter("arguments").invoicePath,
                 model: "invoice"
             });
-        },
-        onNavBack: function() {
-            var oHistory = History.getInstance();
-            var sPreviousHash = oHistory.getPreviousHash();
-
-            if (sPreviousHash !== undefined) {
-                window.history.go(-1);
-            } else {
-                var oRouter = sap.ui.core.UIComponent.getRouteFor(this);
-                oRouter.navTo("overview", {}, true);
-            }
         }
     });
 });
